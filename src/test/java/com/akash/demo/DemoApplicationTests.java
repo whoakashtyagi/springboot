99c1cd48eb0aa1914d2914ab3725a4d8d39bada4
whoakashtyagi <tyagiakash8395990958@gmail.com>
2024-12-13 15:52:19 +0530

    initial checkin

---
+++ src/test/java/com/akash/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
